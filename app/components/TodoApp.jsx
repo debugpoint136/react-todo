2d30dd77c73e403911235e257a00b3ccb10d2bbf
debugpoint136 <dpurushotham136@gmail.com>
2016-05-13 12:08:55 -0500

    Section 9, lecture 86 done

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,36 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp  = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Pay the bills'
+        },
+        {
+          id: 4,
+          text: 'Do Laundry'
+        }
+
+      ]
+    };
+  },
 
   render: function() {
+    var { todos } = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     );
   }
